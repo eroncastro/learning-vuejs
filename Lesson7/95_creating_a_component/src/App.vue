a08f595fb04159d5e054609f3a1ff0b9dc2c2ef3
Eron <eroncastro@gmail.com>
2017-02-24 07:29:26 -0300

    Adds Lesson7/95_creating_a_component

---
+++ Lesson7/95_creating_a_component/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-server-status></app-server-status>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        status: 'Critical'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
+

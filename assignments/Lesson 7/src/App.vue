c6828268432ced5b09753c4dcfe700dae54e174f
Eron <eroncastro@gmail.com>
2017-02-24 08:45:47 -0300

    Adds Lesson 7 assignment

---
+++ assignments/Lesson 7/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <app-servers></app-servers>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+  import Header from './Header.vue'
+  import Servers from './Servers.vue'
+  import Footer from './Footer.vue'
+
+  export default {
+    components: {
+      'app-header': Header,
+      'app-servers': Servers,
+      'app-footer': Footer
+    }
+  }
+</script>
+
+<style>
+
+</style>

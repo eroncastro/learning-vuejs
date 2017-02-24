4313076246ed962660b1e19f347bde3768b022a1
Eron <eroncastro@gmail.com>
2017-02-24 08:45:23 -0300

    Adds Lesson7/96_using_components

---
+++ Lesson7/96_using_components/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-servers></app-servers>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        status: 'Critical'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
---
+++ Lesson7/96_using_components/src/ServerStatus.vue
@@ -0,0 +1,23 @@
+<template>
+  <!-- The component template should contain exactly one root element -->
+  <div>
+    <p>Server Status: {{ status }}</p>
+    <hr>
+    <button @click="changeStatus">Change Status</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function() {
+      return {
+        status: 'Critical'
+      }
+    },
+    methods: {
+      changeStatus() {
+        this.status = 'Normal';
+      }
+    }
+  }
+</script>

1883fe3d4038c8103293baef08bcb3857f2e91cf
Eron <eroncastro@gmail.com>
2017-02-24 08:45:32 -0300

    Adds Lesson7/97_moving_to_a_better_folder_structure

---
+++ Lesson7/97_moving_to_a_better_folder_structure/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <app-servers></app-servers>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+  // One possible setup that is greater for small to medium sized applications
+  // is to store components in a components folder.
+  // For biggers applications, it is preferable to structure folders by feature.
+  import Header from './components/Shared/Header.vue';
+  import Servers from './components/Server/Servers.vue';
+  import Footer from './components/Shared/Footer.vue';
+
+  export default {
+    components: {
+      'app-header': Header,
+      'app-servers': Servers,
+      'app-footer': Footer
+    }
+  }
+</script>
+
+<style>
+
+</style>

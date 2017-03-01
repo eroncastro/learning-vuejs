303fe01e749f7bc18f8e9ac9c1383fa54b9fecaf
Eron <eroncastro@gmail.com>
2017-03-01 13:38:09 -0300

    Adds Lesson7/99_how_to_name_your_components_tags

---
+++ Lesson7/99_how_to_name_your_components_tags/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <app-header></app-header>
+    <hr>
+    <app-servers></app-servers>
+    <hr>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+  /*
+    It is possible to name components using both hyphenated or camel-case.
+    When using camel-case, VueJS will automatically generate the hyphenated version.
+    ES6 also brings the possilibity of declaring both Key and Value by only specifying
+    a object once, like: components: { Servers }
+  */
+  import Header from './components/Shared/Header.vue';
+  import Servers from './components/Server/Servers.vue';
+  import Footer from './components/Shared/Footer.vue';
+
+  export default {
+    components: {
+      'app-header': Header,
+      'app-servers': Servers,
+      'app-footer': Footer
+    }
+  }
+</script>
+
+<style>
+
+</style>

06fa15b1574042d076f812fa97d6bc99ab4e489d
Eron <eroncastro@gmail.com>
2017-03-02 07:54:50 -0300

    Adds Lesson8/105_using_props_for_parent_child_communication

---
+++ Lesson8/105_using_props_for_parent_child_communication/src/components/UserDetail.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ name }}</p>
+  </div>
+</template>
+
+<script>
+  /*
+    For transferring data from the parent to the child we use 'props',
+    which are properties set from outside, from the parent.
+
+    props is an array of strings. Each string is a property that should
+    be set from outside. In order to pass it, we set it as a key value pair
+    where the component is being used.
+  */
+  export default {
+    props: ['name']
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>

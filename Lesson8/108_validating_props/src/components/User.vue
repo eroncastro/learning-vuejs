a0b05585194eeabaffb40f779414e4da092afc2f
Eron <eroncastro@gmail.com>
2017-03-02 08:14:30 -0300

    Adds Lesson8/108_validating_props

---
+++ Lesson8/108_validating_props/src/components/User.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my name</button>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <!--
+          To dynamically pass a property to a component,
+          we use v-bind or its shorhand : (colon)
+         -->
+        <app-user-detail :myName="name"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import UserDetail from './UserDetail.vue';
+  import UserEdit from './UserEdit.vue';
+
+  export default {
+    data: function() {
+      return {
+        name: 'Max'
+      };
+    },
+    methods: {
+      changeName() {
+        this.name = 'Anna';
+      }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightblue;
+  }
+</style>
---
+++ Lesson8/108_validating_props/src/components/UserDetail.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+  </div>
+</template>
+
+<script>
+  /*
+    While using props, we must validate their values in order
+    to prevent the application from breaking.
+
+    To validate props, instead of being an array it must be an object,
+    in which the keys are the props and the values can be: a valid type,
+    an array of valid type or an object.
+
+    A validation error will prevent the component from being mounted.
+  */
+  export default {
+    props: {
+      myName: {
+        type: String,
+        default: 'Max'
+      }
+    },
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("");
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>

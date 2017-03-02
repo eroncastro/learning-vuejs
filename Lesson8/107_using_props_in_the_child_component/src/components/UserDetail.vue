13af5478e498d8b7d923e8ceceb4b5a5a969454a
Eron <eroncastro@gmail.com>
2017-03-02 08:02:44 -0300

    Adds Lesson8/107_using_props_in_the_child_component

---
+++ Lesson8/107_using_props_in_the_child_component/src/components/UserDetail.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+  </div>
+</template>
+
+<script>
+  /*
+    A property supplied with props can be also used
+    inside a component's method.
+  */
+  export default {
+    props: ['myName'],
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("");
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>

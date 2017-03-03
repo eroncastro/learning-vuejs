d878eb74695eae43853c55d5fc850b0ba79d85d5
Eron <eroncastro@gmail.com>
2017-03-03 08:34:08 -0300

    Adds Lesson8/112_communication_between_sibling_components

---
+++ Lesson8/112_communication_between_sibling_components/src/components/UserDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+    <p>User Age: {{ userAge }}</p>
+    <button @click="resetName">Reset name</button>
+    <button @click="resetFn">Reset name</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      myName: {
+        type: String,
+        default: 'Max'
+      },
+      resetFn: Function,
+      userAge: Number
+    },
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("");
+      },
+      resetName() {
+        this.myName = 'Max';
+        this.$emit('nameWasReset', this.myName);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>

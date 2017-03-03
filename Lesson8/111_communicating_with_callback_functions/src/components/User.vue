55c13b5c056d36c79ef9289cf760074daa014dc2
Eron <eroncastro@gmail.com>
2017-03-03 08:18:22 -0300

    Adds Lesson8/111_communicating_with_callback_functions/

---
+++ Lesson8/111_communicating_with_callback_functions/src/components/User.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my name</button>
+    <p>Name is {{ name }}</p>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <!--
+          VueJS uses unidirectional flow communication, which means
+          that the communication is performed between parent and child
+          and it is not directly possible between two siblings.
+         -->
+
+        <!--
+          It is possible to pass a callback as a prop.
+         -->
+        <app-user-detail :myName="name" @nameWasReset="name = $event" :resetFn="resetName"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import UserDetail from './UserDetail.vue';
+  import UserEdit from './UserEdit.vue';
+
+  export default {
+    data: function() {
+      return {
+        name: 'Max'
+      };
+    },
+    methods: {
+      changeName() {
+        this.name = 'Anna';
+      },
+      resetName() {
+        this.name = 'Max';
+      }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightblue;
+  }
+</style>
---
+++ Lesson8/111_communicating_with_callback_functions/src/components/UserDetail.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+    <button @click="resetName">Reset name</button>
+    <button @click="resetFn">Reset name</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      myName: {
+        type: String,
+        default: 'Max'
+      },
+      resetFn: Function
+    },
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("");
+      },
+      resetName() {
+        this.myName = 'Max';
+        // $emit is a way to emit an event that will be passed to the parent component.
+        this.$emit('nameWasReset', this.myName);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>

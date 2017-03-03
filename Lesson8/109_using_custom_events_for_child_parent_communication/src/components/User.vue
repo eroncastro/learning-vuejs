f15f2f81ff7dd004e0c2ecc8369fcc97e2f62adc
Eron <eroncastro@gmail.com>
2017-03-03 07:56:27 -0300

    Adds Lesson8/109_using_custom_events_for_child_parent_communication

---
+++ Lesson8/109_using_custom_events_for_child_parent_communication/src/components/User.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="component">
+    <h1>The User Component</h1>
+    <p>I'm an awesome User!</p>
+    <button @click="changeName">Change my name</button>
+    <p>Name is {{ name }}</p>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <!--
+          To listen for data on a custom event, we use
+          v-on:<custom event> or @<custom event>
+
+          The $event will return the data passed through the event.
+         -->
+        <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
+      </div>
+      <div class="col-xs-12 col-sm-6">
+        <app-user-edit></app-user-edit>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import UserDetail from './UserDetail.vue';
+  import UserEdit from './UserEdit.vue';
+
+  export default {
+    data: function() {
+      return {
+        name: 'Max'
+      };
+    },
+    methods: {
+      changeName() {
+        this.name = 'Anna';
+      }
+    },
+    components: {
+      appUserDetail: UserDetail,
+      appUserEdit: UserEdit
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightblue;
+  }
+</style>
---
+++ Lesson8/109_using_custom_events_for_child_parent_communication/src/components/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="component">
+    <h3>You may view the User Details here</h3>
+    <p>Many Details</p>
+    <p>User Name: {{ switchName() }}</p>
+    <button @click="resetName">Reset name</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      myName: {
+        type: String,
+        default: 'Max'
+      }
+    },
+    methods: {
+      switchName() {
+        return this.myName.split("").reverse().join("");
+      },
+      resetName() {
+        this.myName = 'Max';
+        this.$emit('nameWasReset', this.myName);
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    background-color: lightcoral;
+  }
+</style>

83d271faae5c0c76d4b3b7f427e11e013f4133f2
Eron <eroncastro@gmail.com>
2017-04-24 15:38:50 -0300

    Adds Lesson 17 - 267

---
+++ Lesson_17/267_modularizing_state_management/src/store/store.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import counter from './modules/counter';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    value: 0
+  },
+  getters: {
+    value: state => {
+      return state.value;
+    }
+  },
+  mutations: {
+    updateValue: (state, payload) => {
+      state.value = payload;
+    }
+  },
+  actions: {
+    updateValue: ({ commit }, payload) => {
+      commit('updateValue', payload);
+    }
+  },
+  modules: {
+    counter
+  }
+});

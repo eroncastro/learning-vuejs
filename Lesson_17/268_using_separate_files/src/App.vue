d19e66a1966009090173c7d0fca26b3b53ddf22d
Eron <eroncastro@gmail.com>
2017-04-24 16:07:57 -0300

    Lesson 17 - 268

---
+++ Lesson_17/268_using_separate_files/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vuex</h1>
+        <app-result></app-result>
+        <app-another-result></app-another-result>
+        <hr>
+        <app-counter></app-counter>
+        <app-another-counter></app-another-counter>
+        <hr>
+        <input type="text" v-model="value">
+        <p>{{ value }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Counter from './components/Counter.vue';
+  import AnotherCounter from './components/AnotherCounter.vue';
+  import Result from './components/Result.vue';
+  import AnotherResult from './components/AnotherResult.vue';
+
+  export default {
+    computed: {
+      // For computed properties it is possible to set a getter and a setter.
+      value: {
+        get() {
+          return this.$store.getters.value;
+        },
+        set(value) {
+          this.$store.dispatch('updateValue', value);
+        }
+      }
+    },
+    components: {
+      appCounter: Counter,
+      appAnotherCounter: AnotherCounter,
+      appResult: Result,
+      appAnotherResult: AnotherResult
+    }
+  }
+</script>

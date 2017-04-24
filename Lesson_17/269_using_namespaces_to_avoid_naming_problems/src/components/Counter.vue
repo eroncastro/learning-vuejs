fd405b6fd71f61c684dfbeb2978334c0ecc12dba
Eron <eroncastro@gmail.com>
2017-04-24 16:42:11 -0300

    Adds Lesson 17 - 269

---
+++ Lesson_17/269_using_namespaces_to_avoid_naming_problems/src/components/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="increment(100)">Increment</button>
+    <button class="btn btn-primary" @click="decrement(50)">Decrement</button>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    methods: {
+      /*
+        increment(by) {
+          this.$store.dispatch('increment', by);
+        },
+        decrement() {
+          this.$store.dispatch('decrement');
+        }
+      */
+      ...mapActions([
+        'increment',
+        'decrement'
+      ])
+    }
+  }
+</script>
---
+++ Lesson_17/269_using_namespaces_to_avoid_naming_problems/src/store/types.js
@@ -0,0 +1,3 @@
+export const DOUBLE_COUNTER = 'counter/DOUBLE_COUNTER';
+export const CLICK_COUNTER = 'counter/CLICK_COUNTER';
+export const STRING_COUNTER = 'counter/STRING_COUNTER';

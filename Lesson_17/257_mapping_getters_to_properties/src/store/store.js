e756e6e4bc3a816bfed8dfb89cc3715e25644680
Eron <eroncastro@gmail.com>
2017-04-24 08:51:03 -0300

    Adds Lesson 17 / 257

---
+++ Lesson_17/257_mapping_getters_to_properties/src/store/store.js
@@ -0,0 +1,23 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+// Vuex is Vue state management tool
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    counter: 0 // Initial state
+  },
+  getters: {
+    /*
+      Methods for getting the state out of the store.
+      This helps avoiding code duplication.
+    */
+    doubleCounter: state => {
+      return state.counter * 2;
+    },
+    stringCounter: state => {
+      return state.counter + ' Clicks';
+    }
+  }
+});

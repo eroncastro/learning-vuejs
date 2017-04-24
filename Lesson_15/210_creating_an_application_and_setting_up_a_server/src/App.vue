e5e35075a720aa7908e121cbf5ea125bbc9e9fd8
Eron <eroncastro@gmail.com>
2017-04-24 19:05:07 -0300

    Adds Lesson 15 - 210

---
+++ Lesson_15/210_creating_an_application_and_setting_up_a_server/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div>
+          <label>Username</label>
+          <input class="form-control" type="text" v-model="user.username">
+        </div>
+        <div>
+          <label>Mail</label>
+          <input class="form-control" type="text" v-model="user.email">
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        user: {
+          username: '',
+          email: ''
+        }
+      }
+    },
+    methods: {
+      submit() {
+        const url = 'https://http-request-f80c4.firebaseio.com/data.json';
+        this.$http.post(url, this.user)
+          .then(response => {
+            console.log(response);
+          })
+          .catch(error => {
+            console.log(error);
+          })
+      }
+    }
+  }
+</script>
+
+<style>
+</style>

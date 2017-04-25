b9170c18ef4b9ec4b3cdc5f0f779a9edc8aefbf9
Eron <eroncastro@gmail.com>
2017-04-25 11:43:43 -0300

    Adds Lesson 15 - 215

---
+++ Lesson_15/215_intercepting_responses/src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import VueResource from 'vue-resource';
+import App from './App.vue';
+
+Vue.use(VueResource);
+Vue.http.options.root = 'https://http-request-f80c4.firebaseio.com/data.json';
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method === 'POST') {
+    request.method = 'PUT';
+  }
+  next(response => {
+    response.json = () => { return { messages: response.body } };
+  });
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})

0539f0db54cf67767314e8cf69999281d267eb13
Eron <eroncastro@gmail.com>
2017-04-25 08:55:03 -0300

    Fixes tabs

--- Lesson_15/209_accessing_http_via_vue_resource/src/App.vue
+++ Lesson_15/209_accessing_http_via_vue_resource/src/App.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Http</h1>
-            </div>
-        </div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-    }
+  export default {
+  }
 </script>
 
 <style>

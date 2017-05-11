58b9e280411de69ae08e290d519fe62ee0e8fbaf
Eron <eroncastro@gmail.com>
2017-05-11 19:02:46 -0300

    Adds Lesson 224

---
+++ Lesson_16/224_setting_up_and_loading_routes/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from './App.vue';
+import { routes } from './routes';
+
+// Adds VueRouter plugin to the Vue instance.
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes
+});
+
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+});

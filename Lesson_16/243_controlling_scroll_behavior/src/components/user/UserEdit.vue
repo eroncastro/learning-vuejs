8314908332389c8bc952c36e48fc8be981a0c47e
Eron <eroncastro@gmail.com>
2017-05-12 11:26:27 -0300

    Adds Lesson 243

---
+++ Lesson_16/243_controlling_scroll_behavior/src/components/user/UserEdit.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h3>Edit the User</h3>
+    <p>Locale: {{ $route.query.locale }}</p>
+    <p>Analytics: {{ $route.query.q }}</p>
+    <div style="height: 700px"></div>
+    <p id="data">Some extra Data</p>
+  </div>
+</template>

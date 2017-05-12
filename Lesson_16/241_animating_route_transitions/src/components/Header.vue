eb6418cb556f5b4a3967adc7b0c2b964baa143ab
Eron <eroncastro@gmail.com>
2017-05-12 11:13:48 -0300

    Add lesson 241

---
+++ Lesson_16/241_animating_route_transitions/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <ul class="nav nav-pills">
+    <!-- exact is used for exact match regarding URLs -->
+    <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+    <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
+  </ul>
+</template>

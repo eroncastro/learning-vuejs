30fb99b04a3bb87b98a773cd609420e12b58c91a
Eron <eroncastro@gmail.com>
2017-05-12 16:13:00 -0300

    Removes debugger

--- Lesson_16/247_loading_routes_lazily/src/routes.js
+++ Lesson_16/247_loading_routes_lazily/src/routes.js
@@ -5,25 +5,25 @@ import Header from './components/Header.vue';
 const User = resolve => {
   require.ensure(['./components/user/User.vue'], () => {
     resolve(require('./components/user/User.vue'));
-  }, 'user');
+  });
 };
 
 const UserStart = resolve => {
   require.ensure(['./components/user/UserStart.vue'], () => {
     resolve(require('./components/user/UserStart.vue'));
-  }, 'user');
+  });
 };
 
 const UserDetail = resolve => {
   require.ensure(['./components/user/UserDetail.vue'], () => {
     resolve(require('./components/user/UserDetail.vue'));
-  }, 'user');
+  });
 };
 
 const UserEdit = resolve => {
   require.ensure(['./components/user/UserEdit.vue'], () => {
     resolve(require('./components/user/UserEdit.vue'));
-  }, 'user');
+  });
 };
 
 export const routes = [

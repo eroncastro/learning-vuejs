949f15294a68f3701015d58699b37df6f0846b67
Eron <eroncastro@gmail.com>
2017-05-12 09:07:30 -0300

    Adds Lesson 228

---
+++ Lesson_16/228_navigating_from_code/src/components/user/User.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <hr>
+    <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      navigateToHome() {
+        // trigger navigation
+        this.$router.push({ path: '/' });
+      }
+    }
+  }
+</script>

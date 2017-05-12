a03f956423dcaa31cfb5fe61c130b62c52133cf6
Eron <eroncastro@gmail.com>
2017-05-12 10:33:00 -0300

    Adds lesson 233

---
+++ Lesson_16/233_setting_up_child_routes/src/routes.js
@@ -0,0 +1,18 @@
+import User from './components/user/User.vue';
+import UserStart from './components/user/UserStart.vue';
+import UserDetail from './components/user/UserDetail.vue';
+import UserEdit from './components/user/UserEdit.vue';
+import Home from './components/Home.vue';
+
+export const routes = [
+  { path: '', component: Home },
+  {
+    path: '/user',
+    component: User,
+    children: [
+      { path: '', component: UserStart },
+      { path: ':id', component: UserDetail },
+      { path: ':id/edit', component: UserEdit },
+    ]
+  }
+];

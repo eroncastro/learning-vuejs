2242eac49fc2c7d3060014247e7988f9592093d2
Eron <eroncastro@gmail.com>
2017-05-12 09:21:51 -0300

    Adds Lesson 230

---
+++ Lesson_16/230_fetching_and_using_route_parameters/src/components/user/User.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>The User Page</h1>
+    <hr>
+    <p>Loaded ID: {{ id }}</p>
+    <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        id: this.$route.params.id
+      }
+    },
+    methods: {
+      navigateToHome() {
+        // trigger navigation
+        this.$router.push({ path: '/' });
+      }
+    }
+  }
+</script>

5b909c070d420c598e3cbd5884fb2b33836b5b4b
Eron <eroncastro@gmail.com>
2017-05-12 10:41:41 -0300

    Adds lesson 236

---
+++ Lesson_16/236_named_routes/src/components/user/UserDetail.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>Some User Details</h3>
+    <p>User loaded has ID: {{ $route.params.id }}</p>
+    <router-link
+      tag="button"
+      :to="{ name: 'userEdit', params: { id: $route.params.id } }"
+      class="btn btn-primary">
+      Edit the User
+    </router-link>
+  </div>
+</template>

b34d8a743ba49e69ee149c944d654518b1bf7879
Eron <eroncastro@gmail.com>
2017-05-29 17:40:11 -0300

    Adds Lesson 185

---
+++ Lesson_14/185_creating_a_fade_transition_with_the_css_transition/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br><br>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
+
+<style>
+  /*
+    Vue will analyze this CSS to determine how long the animation will run.
+  */
+
+  /* fade-enter is the class attached for one frame at the beginning. */
+  .fade-enter {
+    opacity: 0; /* transparent */
+  }
+
+  /* This is the class to set the transition. */
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+    opacity: 1;
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+</style>

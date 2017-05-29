fb4ca3df4b6eee6c40a35c7d14cff17c7312526e
Eron <eroncastro@gmail.com>
2017-05-29 17:21:52 -0300

    Fixes explanation

--- Lesson_14/182_preparing_code_to_use_transitions/src/App.vue
+++ Lesson_14/182_preparing_code_to_use_transitions/src/App.vue
@@ -7,21 +7,10 @@
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <br><br>
         <!--
-          One of the typical cases to use the transition component is to attach CSS classes.
-          Vue does this for us: it determines when it should attach an element
-          and then it will attach two CSS classes for animating it in and animating it out.
-          By styling these classes out, we can, therefore, setup an animation.
+          transition is a special element used to animate what is
+          between the opening and the closing tag.
 
-          We animate in an element and when Vue detects that v-if or v-show is true,
-          it animates such element.
-
-          Enter class: <name>-enter
-          Enter-active class: <name>-enter-active
-
-          Leave class: <name>-leave
-          Leave active-class: <name>-leave-active
-
-          Default: v-enter if no name is supplied
+          It is only possible to animate one element with a transition.
         -->
         <transition>
           <div>

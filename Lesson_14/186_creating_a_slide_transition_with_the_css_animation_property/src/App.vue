7d44a1aa581a3902c05acdaa44558e0bc11d5f5c
Eron <eroncastro@gmail.com>
2017-05-29 17:50:30 -0300

    Adds Lesson 186

---
+++ Lesson_14/186_creating_a_slide_transition_with_the_css_animation_property/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br><br>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+        <transition name="slide">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
+
+<style>
+  /*
+    Vue will analyze this CSS to determine how long the animation will run.
+  */
+
+  /* fade-enter is the class attached for one frame at the beginning. */
+  .fade-enter {
+    opacity: 0; /* transparent */
+  }
+
+  /* This is the class to set the transition. */
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+    opacity: 1;
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  .slide-enter {
+    /*transform: translateY(20px);*/
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+  }
+
+  .slide-leave {
+
+  }
+
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translate Y(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
+</style>

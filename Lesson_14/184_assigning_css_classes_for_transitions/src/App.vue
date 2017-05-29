f9a117a98af9726d494ffcf1551e76b9e530e294
Eron <eroncastro@gmail.com>
2017-05-29 17:25:41 -0300

    Adds Lesson 184

---
+++ Lesson_14/184_assigning_css_classes_for_transitions/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br><br>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
+
+<style>
+  /*
+    Vue will analyze this CSS to determine how long the animation will run.
+  */
+  .fade-enter {
+
+  }
+
+  .fade-enter-active {
+
+  }
+
+  .fade-leave {
+
+  }
+
+  .fade-leave-active {
+
+  }
+</style>

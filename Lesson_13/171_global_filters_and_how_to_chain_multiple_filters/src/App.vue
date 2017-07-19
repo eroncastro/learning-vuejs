f8b54a27fc59eb408edb147989f112eba490e086
Eron <eroncastro@gmail.com>
2017-07-19 11:03:36 -0300

    Adds Lecture 171

---
+++ Lesson_13/171_global_filters_and_how_to_chain_multiple_filters/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters &amp; Mixins</h1>
+        <!-- Chaining filters -->
+        <p>{{ text | toUpperCase | to-lowercase }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  /*
+    A filter is a a feature you use in your template to transform an output on it.
+    It does not transform the data itself; rather, it transforms what the user sees.
+  */
+
+  export default {
+    data: function () {
+      return {
+        text: 'Hello there!'
+      }
+    },
+    filters: {
+      // local filter
+      toUpperCase(value) {
+        return value.toUpperCase();
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
+

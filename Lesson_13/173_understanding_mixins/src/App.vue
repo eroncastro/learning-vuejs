88cfc883fd757e36b2714469f0aba34a1b645547
Eron <eroncastro@gmail.com>
2017-07-19 12:09:58 -0300

    Adds Lecture 173

---
+++ Lesson_13/173_understanding_mixins/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters &amp; Mixins</h1>
+        <p>{{ text | toUpperCase | to-lowercase }}</p>
+        <hr>
+        <input v-model="filterText">
+        <ul>
+          <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+        <app-list></app-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import List from './List.vue';
+
+  export default {
+    data: function () {
+      return {
+        text: 'Hello there!',
+        fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+        filterText: ''
+      }
+    },
+    filters: {
+      toUpperCase(value) {
+        return value.toUpperCase();
+      }
+    },
+    computed: {
+      filteredFruits() {
+        return this.fruits.filter((fruit) => fruit.match(this.filterText));
+      }
+    },
+    components: {
+      appList: List
+    }
+  }
+</script>
+
+<style>
+
+</style>
+
---
+++ Lesson_13/173_understanding_mixins/src/List.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h1>Filters &amp; Mixins</h1>
+    <input v-model="filterText">
+    <ul>
+      <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+        filterText: ''
+      }
+    },
+    computed: {
+      filteredFruits() {
+        return this.fruits.filter(fruit => fruit.match(this.filterText));
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
+

d5776cb84bdcd1e1bf213384554ad1642674a0fc
Eron <eroncastro@gmail.com>
2017-09-05 12:49:37 -0300

    Adds Lesson 9 118

---
+++ Lesson9/118_setting_up_the_module_project/src/components/Quote.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>A wonderful quote!</p>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 3px;
+    margin: 30px auto;
+    text-align: center;
+  }
+</style>

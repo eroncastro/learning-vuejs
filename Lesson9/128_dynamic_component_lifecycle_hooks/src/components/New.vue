f1b27dbbed2c10ddda9becea975ba6e766b1cc43
Eron <eroncastro@gmail.com>
2017-09-05 15:18:41 -0300

    Adds Lesson 9 - 128

---
+++ Lesson9/128_dynamic_component_lifecycle_hooks/src/components/New.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Increase!</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0
+      };
+    },
+    destroyed() {
+      // No longer works for our dynamic component.
+      console.log('Destroyed');
+    },
+    deactivated() {
+      console.log('Deactivated!');
+    },
+    activated() {
+      console.log('Activated!');
+    }
+  }
+</script>
+
+<style scoped>
+</style>

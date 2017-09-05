2f8723cdcf29a9fe6b2d4a9ff7df638a2900a76e
Eron <eroncastro@gmail.com>
2017-09-05 14:58:09 -0300

    Improves comment

--- Lesson9/124_a_summary_on_slots/src/App.vue
+++ Lesson9/124_a_summary_on_slots/src/App.vue
@@ -3,6 +3,12 @@
     <div class="row">
       <div class="col-xs-12">
         <app-quote>
+          <!--
+            Slots help us distribute content in other components.
+            It is specially useful whenever we want to build reusable
+            widgets, like a slide show, where we have the frame for each
+            slide and we want to switch between them.
+          -->
           <h2 slot="title">{{ quoteTitle }}</h2>
           <p>A wonderful quote</p>
         </app-quote>

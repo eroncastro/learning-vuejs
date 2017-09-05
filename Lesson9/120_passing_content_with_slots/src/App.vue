2c49746de7e17834b655ce8e6346726aa9d6eb2f
Eron <eroncastro@gmail.com>
2017-09-05 14:16:27 -0300

    Adds Lesson 9 - 120

---
+++ Lesson9/120_passing_content_with_slots/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <!--
+          <app-quote :quote="A wonderful quote!"></app-quote>
+        -->
+        <!--
+          We want to be able to do the following:
+          <app-quote>
+            <h2>The quote</h2>
+            <p>A wonderful Quote</p>
+          </app-quote>
+
+          This is done using slots.
+        -->
+        <app-quote>
+          <h2>The quote</h2>
+          <p>A wonderful quote</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+  export default {
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ Lesson9/120_passing_content_with_slots/src/components/Quote.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['quote']
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 3px;
+    margin: 30px auto;
+    text-align: center;
+  }
+</style>

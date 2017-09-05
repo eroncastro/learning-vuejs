3d03a35fbe4c5bafb6b0ea8f19c570a9580668f7
Eron <eroncastro@gmail.com>
2017-09-05 14:23:36 -0300

    Adds Lesson 9 - 120

---
+++ Lesson9/121_how_slot_content_gets_compiled_and_styled/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <!--
+          <app-quote :quote="A wonderful quote!"></app-quote>
+
+          We want to be able to do the following:
+          <app-quote>
+            <h2>The quote</h2>
+            <p>A wonderful Quote</p>
+          </app-quote>
+
+          This is done using slots.
+
+          The styling is done on the child component;
+          the rest is done inside the parent component.
+        -->
+        <app-quote>
+          <h2>{{ quoteTitle }}</h2>
+          <p>A wonderful quote</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+  export default {
+    data() {
+      return {
+        quoteTitle: 'The Quote'
+      };
+    },
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ Lesson9/121_how_slot_content_gets_compiled_and_styled/src/components/Quote.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 3px;
+    margin: 30px auto;
+    text-align: center;
+  }
+
+  /* This will style the child passed inside the slot tag. */
+
+  h2 {
+    color: red;
+  }
+</style>

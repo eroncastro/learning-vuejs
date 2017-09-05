4f1bb0758c17987db790ec2629d63b85947553ba
Eron <eroncastro@gmail.com>
2017-09-05 14:28:48 -0300

    Adds Lesson 9 - 122

---
+++ Lesson9/122_using_multiple_slots/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p slot="content">A wonderful quote</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+  export default {
+    data() {
+      return {
+        quoteTitle: 'The Quote'
+      };
+    },
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ Lesson9/122_using_multiple_slots/src/components/Quote.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <hr>
+    <div>
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 3px;
+    margin: 30px auto;
+    text-align: center;
+  }
+
+  /* This will style the child passed inside the slot tag. */
+
+  h2 {
+    color: red;
+  }
+
+  .title {
+    font-style: italic;
+  }
+</style>

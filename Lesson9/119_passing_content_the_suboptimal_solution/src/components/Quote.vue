7f5e6b5c04c6711f42b47d42254741ab1f58c54d
Eron <eroncastro@gmail.com>
2017-09-05 14:12:53 -0300

    Adds Lesson 9 - 119

---
+++ Lesson9/119_passing_content_the_suboptimal_solution/src/components/Quote.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid #ccc;
+    box-shadow: 1px 1px 2px black;
+    padding: 3px;
+    margin: 30px auto;
+    text-align: center;
+  }
+</style>

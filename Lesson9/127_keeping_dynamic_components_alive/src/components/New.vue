fc63247ad3db01e4014489d0242952fd07d2be63
Eron <eroncastro@gmail.com>
2017-09-05 15:14:21 -0300

    Adds Lesson 9 - 127

---
+++ Lesson9/127_keeping_dynamic_components_alive/src/components/New.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>New Quote</h3>
+    <button @click="counter++">Increase!</button>
+    <p>{{ counter }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0
+      };
+    },
+    destroyed() {
+      console.log('Destroyed');
+    }
+  }
+</script>
+
+<style scoped>
+</style>

f613c4f51066ac7204c65bb23de4c0d735859574
Eron <eroncastro@gmail.com>
2017-09-06 19:25:41 -0300

    Adds Lesson 11 assignment

---
+++ assignments/Lesson_11/src/App.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="container">
+    <form v-if="!submitted">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <!-- Exercise 1 -->
+          <!-- Create a Signup Form where you retrieve the following Information -->
+          <!-- Full Name (First Name + Last Name) -->
+          <!-- Mail -->
+          <!-- Password -->
+          <!-- Store Data? Yes/No -->
+          <full-name v-model="fullName">
+
+          </full-name>
+
+          <div class="form-group">
+            <label for="mail">Mail</label>
+            <input
+              type="text"
+              id="mail"
+              class="form-control"
+              v-model="mail">
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              v-model="password">
+          </div>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+          Store Data:
+          <label for="yes">
+            <input
+              type="radio"
+              id="yes"
+              value="true"
+              v-model="storeData">Yes
+          </label>
+          <label for="no">
+            <input
+              type="radio"
+              id="no"
+              value="false"
+              v-model="storeData">No
+          </label>
+        </div>
+      </div>
+
+      <!-- Exercise 2 -->
+      <!-- Only display the Form if it has NOT been submitted -->
+      <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+      <!-- Exercise 3 -->
+      <!-- Edit the Example from above and create a custom "Full Name" Control -->
+      <!-- which still holds the First Name and Last Name Input Field -->
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <button
+            class="btn btn-primary"
+            @click.prevent="submitForm">
+            Submit!
+          </button>
+        </div>
+      </div>
+    </form>
+
+    <hr>
+
+    <div
+      class="row"
+      v-if="submitted">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h4>Your Data</h4>
+          </div>
+          <div class="panel-body">
+            <p>Full Name: {{ fullName }}</p>
+            <p>Mail: {{ mail }}</p>
+            <p>Password: {{ password }}</p>
+            <p>Store in Database?: {{ storeData }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import FullName from './FullName.vue';
+
+  export default {
+    data() {
+      return {
+        fullName: '',
+        mail: '',
+        password: '',
+        storeData: false,
+        submitted: false
+      };
+    },
+    methods: {
+      submitForm() {
+        this.submitted = true;
+      }
+    },
+    components: {
+      FullName
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ assignments/Lesson_11/src/FullName.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form-group">
+    <label for="fullname">Full Name</label>
+    <input
+      type="text"
+      id="fullname"
+      class="form-control"
+      :value="value"
+      @input="inputChanged($event.target.value)">
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: {
+        type: String,
+        default: '',
+        required: false
+      }
+    },
+    methods: {
+      inputChanged(newValue) {
+        this.$emit('input', newValue)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>

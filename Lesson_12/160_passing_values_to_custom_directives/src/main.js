5ee8372a2963ff570aae60c6ee6f017b56a7f30f
Eron <eroncastro@gmail.com>
2017-09-11 15:05:35 -0300

    Adds Lesson 12 - 160

---
+++ Lesson_12/160_passing_values_to_custom_directives/src/main.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+// Global directive
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    // el.style.backgroundColor = 'green';
+    el.style.backgroundColor = binding.value;
+  }
+});
+
+/*
+  A directive has five hooks:
+
+  - bind(el, binding, vnode) -> once directive is attached
+  - inserted(el, binding, vnode) -> inserted in parent node
+  - update(el, binding, vnode, oldVnode) -> once component is updated without children
+  - componentUpdated(el, binding, vnode, oldVnode) -> once component is updated with children
+  - unbind(el, binding, vnode) -> once directive is removed
+*/
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})

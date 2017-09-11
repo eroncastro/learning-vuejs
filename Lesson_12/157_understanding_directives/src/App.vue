54b0bb294d997bdcdbad023a399cbd930524dedd
Eron <eroncastro@gmail.com>
2017-09-11 14:48:48 -0300

    Adds Lesson 12 - 157

---
+++ Lesson_12/157_understanding_directives/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <!--
+          Directives in VueJS have the form v-<directiveName>
+          Important: When using v-html, it is importang to sanitize your string
+          not to get XSS attack.
+        -->
+        <h1>Build-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'<strong>Some strong text</strong>'"></p>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+
+</style>

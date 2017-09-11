14e1015e9ad119b2692a12bde195af38ffcecd68
Eron <eroncastro@gmail.com>
2017-09-11 19:08:54 -0300

    Adds Lesson 10 - 138

---
+++ Lesson_10/138_adding_an_info_box/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on a quote to delete it!
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+
+  export default {
+    data() {
+      return {
+        quotes: [
+          'Just a quote to see something'
+        ],
+        maxQuotes: 10
+      };
+    },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote);
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote
+    }
+  }
+</script>
+
+<style>
+</style>

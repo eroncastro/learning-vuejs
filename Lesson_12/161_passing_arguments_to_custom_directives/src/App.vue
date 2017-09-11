18ecb0447498bb3381fa7979456865e6473a3c65
Eron <eroncastro@gmail.com>
2017-09-11 16:30:07 -0300

    Add Lesson 12 - 161

---
+++ Lesson_12/161_passing_arguments_to_custom_directives/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <!--
+          Directives in VueJS have the form v-<directiveName>
+          Important: When using v-html, it is importang to sanitize your string
+          not to get XSS attack.
+        -->
+        <h1>Build-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'<strong>Some strong text</strong>'"></p>
+      </div>
+      <hr>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <!--
+          binding is the information about the directive that goes between the
+          quotation marks after the directive.
+
+          To pass an argument to a directive, we use colon, like v-bind:attr
+        -->
+        <p v-highlight:background="'red'">Color this</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+
+</style>

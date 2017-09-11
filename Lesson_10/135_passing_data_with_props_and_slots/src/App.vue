01289b85c4cb084bcedd39a85da6fbe9a3127efe
Eron <eroncastro@gmail.com>
2017-09-11 18:53:01 -0300

    Adds Lesson 10 - 135

---
+++ Lesson_10/135_passing_data_with_props_and_slots/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+
+  export default {
+    data() {
+      return {
+        quotes: [
+          'Just a quote to see something'
+        ],
+        maxQuotes: 10
+      };
+    },
+    components: {
+      appQuoteGrid: QuoteGrid
+    }
+  }
+</script>
+
+<style>
+</style>

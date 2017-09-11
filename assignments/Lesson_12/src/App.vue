a91be1689a605e6c9ce07ec8d33eef21f3ed0a9b
Eron <eroncastro@gmail.com>
2017-09-11 17:27:34 -0300

    Adds Lesson 12 assignment

---
+++ assignments/Lesson_12/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Directives Exercise</h1>
+        <!-- Exercise -->
+        <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+        <button v-myOn:click="clicked">Click me!</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    directives: {
+      myOn: {
+        bind(el, binding) {
+          el.addEventListener(binding.arg, binding.value);
+        }
+      }
+    },
+    methods: {
+      clicked() {
+        alert('button clicked');
+      }
+    }
+  }
+</script>
+
+<style>
+</style>

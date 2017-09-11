26c02bc6173da1c43a635743193aa684fd45a4ca
Eron <eroncastro@gmail.com>
2017-09-11 18:48:09 -0300

    Adds Lesson 10 - 134

---
+++ Lesson_10/134_creating_the_application_components/src/components/QuoteGrid.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      quotes: {
+        type: Array,
+        required: false,
+        default: () => []
+      }
+    }
+    components: {
+
+    }
+  }
+</script>
+
+<style>
+
+</style>

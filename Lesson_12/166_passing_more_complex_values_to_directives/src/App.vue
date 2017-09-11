841916e631011c90eeac4028e23452e8ce74870a
Eron <eroncastro@gmail.com>
2017-09-11 17:12:19 -0300

    Adds Lesson 12 - 166

---
+++ Lesson_12/166_passing_more_complex_values_to_directives/src/App.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <!--
+          Directives in VueJS have the form v-<directiveName>
+          Important: When using v-html, it is importang to sanitize your string
+          not to get XSS attack.
+        -->
+        <h1>Build-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'<strong>Some strong text</strong>'"></p>
+      </div>
+      <hr>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <!--
+          binding is the information about the directive that goes between the
+          quotation marks after the directive.
+
+          To pass an argument to a directive, we use colon, like v-bind:attr.
+          To apply a modifier, we use the dot notation after the argument.
+          Important: modifiers can be chained together.
+        -->
+        <p v-highlight:background.delayed="'red'">Color this</p>
+        <p v-local-highlight:background.delayed.blink="{ mainColor: 'red', secondColor: 'green', delay: 500 }">Color this, too</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  // Local directives are registered like local components.
+  export default {
+    directives: {
+      'local-highlight': {
+        bind(el, binding, vnode) {
+          // el.style.backgroundColor = 'green';
+          // el.style.backgroundColor = binding.value;
+          let delay = 0;
+
+          if (binding.modifiers['delayed']) {
+            delay = 3000;
+          }
+
+          if (binding.modifiers['blink']) {
+            let mainColor = binding.value.mainColor;
+            let secondColor = binding.value.secondColor;
+            let currentColor = mainColor;
+
+            setTimeout(() => {
+              setInterval(() => {
+                currentColor = currentColor === secondColor ?
+                               mainColor :
+                               secondColor;
+              if (binding.arg === 'background') {
+                el.style.backgroundColor = currentColor;
+              } else {
+                el.style.color = currentColor;
+              }
+              }, binding.value.delay);
+            }, delay);
+          } else {
+            setTimeout(() => {
+              if (binding.arg === 'background') {
+                el.style.backgroundColor = binding.value.mainColor;
+              } else {
+                el.style.color = binding.value.mainColor;
+              }
+            }, delay);
+          }
+        }
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>

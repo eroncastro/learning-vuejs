7a388d6582a05208c1579abb5e52fa9d0f349413
Eron <eroncastro@gmail.com>
2017-09-11 14:49:34 -0300

    Adds hr

--- Lesson_12/157_understanding_directives/src/App.vue
+++ Lesson_12/157_understanding_directives/src/App.vue
@@ -10,7 +10,11 @@
         <h1>Build-in Directives</h1>
         <p v-text="'Some Text'"></p>
         <p v-html="'<strong>Some strong text</strong>'"></p>
-
+      </div>
+      <hr>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p>Color this</p>
       </div>
     </div>
   </div>

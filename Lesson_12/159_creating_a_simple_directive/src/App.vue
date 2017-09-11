d345852124dbee47a67a86233bd2a2fe20596cfa
Eron <eroncastro@gmail.com>
2017-09-11 14:59:29 -0300

    Adds Lesson 12 - 159

---
+++ Lesson_12/159_creating_a_simple_directive/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <!--
+          Directives in VueJS have the form v-<directiveName>
+          Important: When using v-html, it is importang to sanitize your string
+          not to get XSS attack.
+        -->
+        <h1>Build-in Directives</h1>
+        <p v-text="'Some Text'"></p>
+        <p v-html="'<strong>Some strong text</strong>'"></p>
+      </div>
+      <hr>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Custom Directives</h1>
+        <p v-highlight>Color this</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style>
+
+</style>

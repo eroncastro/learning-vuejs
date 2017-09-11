bd6103b47a6e0b0e22533e310989696218838a19
Eron <eroncastro@gmail.com>
2017-09-11 19:06:04 -0300

    Adds Lesson 10 - 137

---
+++ Lesson_10/137_adding_quotes_with_custom_events/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+
+  export default {
+    data() {
+      return {
+        quotes: [
+          'Just a quote to see something'
+        ],
+        maxQuotes: 10
+      };
+    },
+    methods: {
+      newQuote(quote) {
+        this.quotes.push(quote);
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote
+    }
+  }
+</script>
+
+<style>
+</style>

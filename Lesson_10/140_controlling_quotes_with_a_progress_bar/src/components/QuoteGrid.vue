ea63b2c4a7379133b8ac610b71f566817fd8ac21
Eron <eroncastro@gmail.com>
2017-09-12 09:42:58 -0300

    Adds Lesson 10 -140

---
+++ Lesson_10/140_controlling_quotes_with_a_progress_bar/src/components/QuoteGrid.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row">
+    <!--
+      The native modifier tells VueJS to react to a click on a component
+      if it happens on the native elements of the component.
+    -->
+    <app-quote
+      v-for="(quote, index) in quotes"
+      @click.native="deleteQuote">
+      {{ quote }}
+    </app-quote>
+  </div>
+</template>
+
+<script>
+  import Quote from './Quote.vue';
+
+  export default {
+    props: {
+      quotes: {
+        type: Array,
+        required: false,
+        default: () => []
+      }
+    },
+    methods: {
+      deleteQuote(index) {
+        this.$emit('quoteDeleted', index);
+      }
+    },
+    components: {
+      appQuote: Quote
+    }
+  }
+</script>
+
+<style>
+
+</style>

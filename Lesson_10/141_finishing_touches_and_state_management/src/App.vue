d879e46fba0f0e8eda7c2ba1d87d04f62e353875
Eron <eroncastro@gmail.com>
2017-09-12 10:00:18 -0300

    Adds Lesson 10 - 141

---
+++ Lesson_10/141_finishing_touches_and_state_management/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <app-header
+      :quoteCount="quotes.length"
+      :maxQuotes="maxQuotes">
+    </app-header>
+
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+
+    <app-quote-grid
+      :quotes="quotes"
+      @quoteDeleted="deleteQuote">
+    </app-quote-grid>
+
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">
+          Info: Click on a quote to delete it!
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import QuoteGrid from './components/QuoteGrid.vue';
+  import NewQuote from './components/NewQuote.vue';
+  import Header from './components/Header.vue'
+
+  export default {
+    data() {
+      return {
+        quotes: [
+          'Just a quote to see something'
+        ],
+        maxQuotes: 10
+      };
+    },
+    methods: {
+      newQuote(quote) {
+        if (this.quotes.length >= this.maxQuotes) {
+          alert('Please, delete some quotes first.');
+          return;
+        }
+        this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header
+    }
+  }
+</script>
+
+<style>
+</style>

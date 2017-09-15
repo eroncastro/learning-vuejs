c3ad4ab461c79f258841c0e986d2d39cb4e91458
Eron <eroncastro@gmail.com>
2017-09-15 10:52:43 -0300

    Adds Lesson 14 - 189

---
+++ Lesson_14/189_setting_up_an_initial_animation/src/App.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr>
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br><br>
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+        <!--
+          We use type to setup which one (animation or transition) to determine
+          which one will dictate both to come to an end.
+        -->
+        <transition name="slide" type="animation">
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+        <!--
+          To tell VueJS to animate the initial attach to the DOM, we can add
+          appear attribute to the transition element.
+        -->
+        <transition name="fade" appear>
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        show: false
+      }
+    }
+  }
+</script>
+
+<style>
+  /*
+    Vue will analyze this CSS to determine how long the animation will run.
+  */
+
+  /* fade-enter is the class attached for one frame at the beginning. */
+  .fade-enter {
+    opacity: 0; /* transparent */
+  }
+
+  /* This is the class to set the transition. */
+  .fade-enter-active {
+    transition: opacity 1s;
+  }
+
+  .fade-leave {
+    opacity: 1;
+  }
+
+  .fade-leave-active {
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  .slide-enter {
+    opacity: 0;
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+    transition: opacity .5s;
+  }
+
+  .slide-leave {
+
+  }
+
+  /*
+    When Vue JS will take the longest duration
+    whenever we have both transition and animation.
+  */
+  .slide-leave-active {
+    animation: slide-out 1s ease-out forwards;
+    transition: opacity 3s;
+    opacity: 0;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translate Y(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(20px);
+    }
+  }
+</style>

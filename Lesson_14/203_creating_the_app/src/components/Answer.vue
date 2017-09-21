293bc00a836d47a83c0eadd0181d9ab16248835e
Eron <eroncastro@gmail.com>
2017-09-21 19:09:56 -0300

    Adds beginning of Lesson 14 - 203

---
+++ Lesson_14/203_creating_the_app/src/components/Answer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="alert alert-success text-center">
+    <h1>That's Correct!</h1>
+    <hr>
+    <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+  </div>
+</template>
+
+<script>
+  export default{
+    methods: {
+      onNextQuestion() {
+        this.$emit('confirmed');
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>

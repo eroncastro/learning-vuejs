bfecc5701f8d81ee3e4d167af04ce10103cc307f
Eron <eroncastro@gmail.com>
2017-09-25 18:22:56 -0300

    Adds Lesson 18 - 282

---
+++ Lesson_18/282_adding_a_portfolio_module_to_vuex/src/store/modules/stocks.js
@@ -0,0 +1,37 @@
+import stocks from '../../data/stocks';
+
+const state = {
+  stocks: []
+};
+
+const getters = {
+  stocks: state => state.stocks
+};
+
+const mutations = {
+  'SET_STOCKS'(state, stocks) {
+    state.stocks = stocks;
+  },
+  'RND_STOCKS'(state) {
+
+  }
+};
+
+const actions = {
+  buyStock: ({ commit }, order) => {
+    commit();
+  },
+  initStocks: (context) => {
+    context.commit('SET_STOCKS', stocks);
+  },
+  randomizeStocks: ({ commit }) => {
+    commit('RND_STOCKS');
+  }
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+};

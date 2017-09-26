8b8a3e0fe9cf26d35d9fb77fb0c94b87307141d6
Eron <eroncastro@gmail.com>
2017-09-26 09:56:43 -0300

    Adds Lesson 18 - 295

---
+++ Lesson_18/295_testing_and_bug_fixes/src/store/actions.js
@@ -0,0 +1,17 @@
+import Vue from 'vue';
+
+export const loadData = ({ commit }) => {
+  Vue.http
+    .get('data.json')
+    .then(response => response.json())
+    .then(data => {
+      if (data) {
+        console.log(data);
+        const { stocks, funds, stockPortfolio } = data;
+        const portfolio = { stockPortfolio, funds };
+
+        commit('SET_STOCKS', stocks);
+        commit('SET_PORTFOLIO', portfolio);
+      }
+    });
+};
---
+++ Lesson_18/295_testing_and_bug_fixes/src/store/modules/portfolio.js
@@ -0,0 +1,58 @@
+const state = {
+  funds: 10000,
+  stocks: []
+};
+
+const getters = {
+  stockPortfolio(state, getters) {
+    return state.stocks.map(stock => {
+      const record = getters.stocks.find(element => element.id === stock.id);
+
+      return Object.assign({}, stock, record);
+    })
+  },
+  funds(state) {
+    return state.funds;
+  }
+};
+
+const mutations = {
+  'BUY_STOCK'(state, { stockId, quantity, stockPrice }) {
+    const record = state.stocks.find(stock => stock.id === stockId);
+
+    if (record) {
+      record.quantity += quantity;
+    } else {
+      state.stocks.push({ id: stockId, quantity });
+    }
+
+    state.funds -= stockPrice * quantity;
+  },
+  'SELL_STOCK'(state, { stockId, quantity, stockPrice }) {
+    const record = state.stocks.find(stock => stock.id === stockId);
+
+    if (record.quantity > quantity) {
+      record.quantity -= quantity;
+    } else {
+      state.stocks.splice(state.stocks.indexOf(record), 1);
+    }
+    state.funds += stockPrice * quantity;
+  },
+  'SET_PORTFOLIO'(state, portfolio) {
+    state.funds = portfolio.funds;
+    state.stocks = portfolio.stockPortfolio ? portfolio.stockPortfolio : [];
+  }
+};
+
+const actions = {
+  sellStock: ({ commit }, order) => {
+    commit('SELL_STOCK', order);
+  }
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+};

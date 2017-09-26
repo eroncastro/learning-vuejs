502f1fdcea96222cd77505d5f897877b006861e5
Eron <eroncastro@gmail.com>
2017-09-26 08:20:13 -0300

    Adds Lesson 18 - 286

---
+++ Lesson_18/286_displaying_the_funds/src/components/portfolio/Stock.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }} | Quantity: {{ stock.quantity }})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="quantity"
+            v-model.number="quantity">
+        </div>
+        <div class="pull-right">
+          <button
+            class="btn btn-success"
+            @click="placeSellOrder"
+            :disabled="this.quantity <= 0 || !Number.isInteger(this.quantity)">
+            Sell
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    props: {
+      stock: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        quantity: 0
+      };
+    },
+    computed: {
+      order() {
+        return {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        };
+      }
+    },
+    methods: {
+      ...mapActions(['sellStock']),
+      placeSellOrder() {
+        this.sellStock(this.order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
---
+++ Lesson_18/286_displaying_the_funds/src/components/stocks/Stock.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="quantity"
+            v-model.number="quantity">
+        </div>
+        <div class="pull-right">
+          <button
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="this.quantity <= 0 || !Number.isInteger(this.quantity)">
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      stock: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        quantity: 0
+      };
+    },
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        };
+
+        this.$store.dispatch('buyStock', order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>

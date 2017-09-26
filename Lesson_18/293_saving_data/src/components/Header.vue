68644681ab07d7c5edfac136e56a23922fcf58f5
Eron <eroncastro@gmail.com>
2017-09-26 09:37:39 -0300

    Adds Lesson 18 - 293

---
+++ Lesson_18/293_saving_data/src/components/Header.vue
@@ -0,0 +1,75 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+      </div>
+
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+          <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+        </ul>
+
+        <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
+
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#" @click="endDay">End Day</a></li>
+          <li
+            class="dropdown"
+            :class="{ open: isOpen }"
+            @click="isOpen = !isOpen">
+            <a
+              href="#"
+              class="dropdown-toggle"
+              data-toggle="dropdown"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false">
+              Save and Load
+              <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="#" @click="saveData">Save Data</a></li>
+              <li><a href="#">Load Data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    data() {
+      return {
+        isOpen: false
+      };
+    },
+    computed: {
+      funds() {
+        return this.$store.getters.funds;
+      },
+    },
+    methods: {
+      ...mapActions([
+        'randomizeStocks'
+      ]),
+      endDay() {
+        this.randomizeStocks();
+      },
+      saveData() {
+        const data = {
+          funds: this.$store.getters.funds,
+          stockPortfolio: this.$store.getters.stockPortfolio,
+          stocks: this.$store.getters.stocks
+        };
+
+        this.$http.put('data.json', data);
+      }
+    }
+  }
+</script>

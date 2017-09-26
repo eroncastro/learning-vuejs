eedfe0a737e691e1ae1e4c7ab40a1940bda29a04
Eron <eroncastro@gmail.com>
2017-09-26 08:29:33 -0300

    Adds Lesson 18 - 287

---
+++ Lesson_18/287_adding_some_order_checks/src/components/stocks/Stock.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-success">
+      <div class="panel-heading">
+        <h3 class="panel-title">
+          {{ stock.name }}
+          <small>(Price: {{ stock.price }})</small>
+        </h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            placeholder="quantity"
+            v-model.number="quantity">
+        </div>
+        <div class="pull-right">
+          <button
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="validatePurchase">
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      stock: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        quantity: 0
+      };
+    },
+    computed: {
+      funds() {
+        return this.$store.getters.funds;
+      },
+      validatePurchase() {
+        return (this.quantity <= 0 ||
+                !Number.isInteger(this.quantity) ||
+                this.quantity * this.stock.price > this.funds);
+      }
+    },
+    methods: {
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        };
+
+        this.$store.dispatch('buyStock', order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
